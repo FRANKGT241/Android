4dd5c56111c5688c54e037b0c6631947f6e0a327
mendel-l <matt.ekisde@gmail.com>
2024-07-16 15:54:52 -0600

    primera parte

--- Dispositivosmoviles/app/src/main/java/com/example/firstappandroid/MainActivity.kt
+++ Dispositivosmoviles/app/src/main/java/com/example/firstappandroid/MainActivity.kt
@@ -20,12 +20,49 @@ class MainActivity : AppCompatActivity() {
         val buttonEnviar: Button = findViewById(R.id.buttonEnviar)
         val editnombre: EditText = findViewById(R.id.editnombre)
 
+        val editapellido: EditText = findViewById(R.id.editapellido)
+        val edittelefono: EditText = findViewById(R.id.edittelefono)
+        val editedad: EditText = findViewById(R.id.editEdad)
+        val editcorreo: EditText = findViewById(R.id.editcorreo)
+
         buttonEnviar.setOnClickListener {
             val name = editnombre.text.toString()
             val intent = Intent(this, SegundaActivity2::class.java)
             intent.putExtra("Nombre", name)
             startActivity(intent)
             Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonEnviar.setOnClickListener {
+            val name = editapellido.text.toString()
+            val intent = Intent(this, SegundaActivity2::class.java)
+            intent.putExtra("Apellido", name)
+            startActivity(intent)
+            //Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonEnviar.setOnClickListener {
+            val name = edittelefono.text.toString()
+            val intent = Intent(this, SegundaActivity2::class.java)
+            intent.putExtra("Telefono", name)
+            startActivity(intent)
+            //Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonEnviar.setOnClickListener {
+            val name = editedad.text.toString()
+            val intent = Intent(this, SegundaActivity2::class.java)
+            intent.putExtra("Edad", name)
+            startActivity(intent)
+            //Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonEnviar.setOnClickListener {
+            val name = editcorreo.text.toString()
+            val intent = Intent(this, SegundaActivity2::class.java)
+            intent.putExtra("Correo", name)
+            startActivity(intent)
+            //Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
         }
     }
 }
